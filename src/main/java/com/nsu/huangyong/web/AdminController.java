cd5fe5f0864010bb69d3eaa143205c237c3d4810
huangyong98k <huangyong.nsu@qq.com>
2018-05-10 20:04:29 +0800

    加入h5页面

--- src/main/java/com/nsu/huangyong/web/AdminController.java
+++ src/main/java/com/nsu/huangyong/web/AdminController.java
@@ -1,15 +1,19 @@
 package com.nsu.huangyong.web;
 
 import com.nsu.huangyong.common.constant.SysInfo;
+import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Slf4j
 @RestController
+@Api(value = "API-base-service",description = "服务根响应")
 public class AdminController {
     /**
      * 当前服务的根响应
@@ -22,5 +26,9 @@ public class AdminController {
         infoMap.put("desc", "水果商城（huangyong.nsu@qq.com）");
         infoMap.put("version", SysInfo.VERSION.getValue());
         return infoMap;
+    }
+    @RequestMapping("/home")
+    public void home(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        response.sendRedirect("/shop/tpl/index.html");
     }
 }
